da683fb82c1fe82a73d76c5a23f2f41ed8e839db
IahhMoments <isaiahchhay0@gmail.com>
2026-06-27 10:18:46 -0400

    downloaded Shadcn
    updated my tree structure for better organization

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+// /src/app/layout.tsx
+import { Toaster } from '@/components/ui/sonner'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  )
+}
